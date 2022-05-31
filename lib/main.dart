698859192197f6b5b6a944f1ec3cceb08a2fb56d
nodir <nodirbarotov0707@gmail.com>
2022-05-31 12:04:02 +0500

    ScreenUtil added and nav bar changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:worshapp/animated.dart';
 import 'package:worshapp/provider/connect_to_firebae.dart';
 import 'package:worshapp/screens/home_page/hj.dart';
 import 'package:worshapp/screens/library/library_page.dart';
-import 'package:worshapp/widgets/song_item.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -18,23 +17,33 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     ConnectToFirebase data = ConnectToFirebase();
     data.addUser();
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Worshapp Demo'),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+      child: const SafeArea(child: MyHomePage()),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -43,11 +52,34 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
   final _inactiveColor = Colors.grey;
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
 
   @override
   void initState() {
     Firebase.initializeApp().whenComplete(() {
-      print("completed");
       setState(() {});
     });
     // TODO: implement initState
@@ -57,15 +89,34 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: _buildBottomNavigationBar(),
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          widget.title,
-          style: TextStyle(color: Colors.white),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xF50093F5),
+        items: const [
+          BottomNavigationBarItem(
+            backgroundColor: Color(0xF50093F5),
+            icon: Icon(Icons.library_books),
+            label: 'Библиотека',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color(0xF50093F5),
+            icon: Icon(Icons.list),
+            label: 'Список',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color(0xF50093F5),
+            icon: Icon(Icons.add),
+            label: 'Добавить',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color(0xF50093F5),
+            icon: Icon(Icons.favorite_border),
+            label: 'Избранное',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
-      body: getBody(),
+      body: _widgetOptions.elementAt(_selectedIndex),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
@@ -78,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       Container(
         alignment: Alignment.center,
-        child: LibraryPage(),
+        child: const LibraryPage(),
       ),
       // Container(
       //   alignment: Alignment.center,
@@ -105,28 +156,28 @@ class _MyHomePageState extends State<MyHomePage> {
       onItemSelected: (index) => setState(() => _currentIndex = index),
       items: <BottomNavyBarItem>[
         BottomNavyBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Программы'),
+          icon: const Icon(Icons.home),
+          title: const Text('Программы'),
           activeColor: Colors.blue,
           inactiveColor: _inactiveColor,
           textAlign: TextAlign.center,
         ),
         BottomNavyBarItem(
-          icon: Icon(Icons.list_alt),
-          title: Text('Библиотека'),
+          icon: const Icon(Icons.list_alt),
+          title: const Text('Библиотека'),
           activeColor: Colors.blue,
           inactiveColor: _inactiveColor,
           textAlign: TextAlign.center,
         ),
-        // BottomNavyBarItem(
-        //   icon: Icon(Icons.person),
-        //   title: Text(
-        //     'Profile',
-        //   ),
-        //   activeColor: Colors.blue,
-        //   inactiveColor: _inactiveColor,
-        //   textAlign: TextAlign.center,
-        // ),
+        BottomNavyBarItem(
+          icon: const Icon(Icons.person),
+          title: const Text(
+            'Profile',
+          ),
+          activeColor: Colors.blue,
+          inactiveColor: _inactiveColor,
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
--- lib/screens/home_page/hj.dart
+++ lib/screens/home_page/hj.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:worshapp/screens/home_page/widgets/any_card.widget.dart';
-import 'package:worshapp/screens/home_page/widgets/latest_worships.widget.dart';
-import 'package:worshapp/screens/home_page/widgets/sunday_program.widget.dart';
-import 'package:worshapp/screens/home_page/widgets/tutorial.widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,166 +11,51 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0xff384850),
-            Color(0xff263238),
-            Color(0xff263238),
-            //Color(0xff61e88a),
-          ],
-        ),
-      ),
-      child: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const SundayProgrammWidget()));
-                    },
-                    child: Card(
-                      color: const Color(0xFF5D88D7),
-                      child: Column(
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Icon(
-                              Icons.directions_run,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Text(
-                              'Воскресная прорамма',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      elevation: 3,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const LatestWorshipSongsWidget()));
-                    },
-                    child: Card(
-                      color: const Color(0xFF5D88D7),
-                      child: Column(
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.all(9),
-                            child: Icon(Icons.account_balance,color: Colors.white,),
-                            ),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
-                          Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Text(
-                              'Новые',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      elevation: 3,
-                    ),
-                  ),
+    print('Width : $width');
+
+    print('Height : $height');
+
+    return Container(
+      color: const Color(0xF50093F5),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: const [
+                Spacer(),
+                Center(
+                    child: Text(
+                  'Библиотека',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                )),
+                Spacer(),
+                Icon(
+                  Icons.power_settings_new,
+                  color: Colors.white,
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Container(
+            height: 50,
+            width: 335,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: const Color(0xFFFFFFFF),
+              shape: BoxShape.rectangle,
+            ),
+            child: Row(
               children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Tutorials()));
-                    },
-                    child: Card(
-                      color: const Color(0xFF5D88D7),
-                      child: Column(
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Icon(
-                              Icons.event,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Text(
-                              'Уроки',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      elevation: 3,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const AnyCardWidget()));
-                    },
-                    child: Card(
-                      color: const Color(0xFF5D88D7),
-                      child: Column(
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Icon(
-                              Icons.home_work_outlined,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Text(
-                              'Работаю из дома',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      elevation: 3,
-                    ),
-                  ),
-                ),
+                const Icon(Icons.search),
+                TextFormField(),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
